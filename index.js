a83fddaf0d811eee9471fef62372d1130727cfcb
Abinash Mohanty <abinash.mohanty@gmail.com>
2017-04-12 17:57:48 +0530

    updated to original index file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 //Slack configuration for team  
 
-/*
+
 // Uses the slack button feature to offer a real time bot to multiple teams 
 var Botkit = require('./lib/Botkit.js');
 
@@ -25,35 +25,6 @@ var controller = Botkit.slackbot({
     scopes: ['bot'],
   }
 );
-*/
-
-var Botkit = require('botkit')
-
-var token = process.env.SLACK_TOKEN
-
-var controller = Botkit.slackbot({
-  // reconnect to Slack RTM when connection goes bad
-  retry: Infinity,
-  debug: false
-})
-
-// Assume single team mode if we have a SLACK_TOKEN
-if (token) {
-  console.log('Starting in single-team mode')
-  controller.spawn({
-    token: token
-  }).startRTM(function (err, bot, payload) {
-    if (err) {
-      throw new Error(err)
-    }
-
-    console.log('Connected to Slack RTM')
-  })
-// Otherwise assume multi-team mode - setup beep boop resourcer connection
-} else {
-  console.log('Starting in Beep Boop multi-team mode')
-  require('beepboop-botkit').start(controller, { debug: true })
-}
 
 
 controller.setupWebserver(process.env.port,function(err,webserver) {
